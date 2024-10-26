3b2526583f4d47ff78a264ca7835f420ab6fa323
NT <109881493+NetThi123@users.noreply.github.com>
2024-10-26 13:31:16 -0400

    Create AppleDelegate.swift

---
+++ AppleDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        let router = AppRouter(window: window!,
+                               controllerFactory: ViewControllerFactory())
+        router.route(to: .reminders)
+        return true
+    }
+}
